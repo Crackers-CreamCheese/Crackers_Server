f3f9f331fe94227245ca0247988cb8dfa1d0265a
Taeyoung Kim <lucy_ia@ewhain.net>
2023-03-23 21:35:25 +0900

    feat: initial setting

---
+++ src/main/java/com/creamcheese/crackers/CrackersApplication.java
@@ -0,0 +1,13 @@
+package com.creamcheese.crackers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackersApplication.class, args);
+	}
+
+}
